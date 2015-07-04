d67ca0f497d6f63060908b88e7275c05a3b10e1e
Constantin Rack <constantin@rack.li>
2015-07-04 10:49:55 +0200

    Add ProPlus files

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"IPv6\"]/address/text()'" <O365IPAddresses.xml >o365_IPv6
-xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"IPv4\"]/address/text()'" <O365IPAddresses.xml >o365_IPv4
-xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"URL\"]/address/text()'"  <O365IPAddresses.xml >o365_URL
+xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"IPv6\"]/address/text()'"     <O365IPAddresses.xml >o365_IPv6
+xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"IPv4\"]/address/text()'"     <O365IPAddresses.xml >o365_IPv4
+xmlsh -c "xpath '/products/product[@name=\"o365\"]/addresslist[@type=\"URL\"]/address/text()'"      <O365IPAddresses.xml >o365_URL
 
-xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"IPv6\"]/address/text()'"  <O365IPAddresses.xml >LYO_IPv6
-xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"IPv4\"]/address/text()'"  <O365IPAddresses.xml >LYO_IPv4
-xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"URL\"]/address/text()'"   <O365IPAddresses.xml >LYO_URL
+xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"IPv6\"]/address/text()'"      <O365IPAddresses.xml >LYO_IPv6
+xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"IPv4\"]/address/text()'"      <O365IPAddresses.xml >LYO_IPv4
+xmlsh -c "xpath '/products/product[@name=\"LYO\"]/addresslist[@type=\"URL\"]/address/text()'"       <O365IPAddresses.xml >LYO_URL
+
+xmlsh -c "xpath '/products/product[@name=\"ProPlus\"]/addresslist[@type=\"IPv6\"]/address/text()'"  <O365IPAddresses.xml >ProPlus_IPv6
+xmlsh -c "xpath '/products/product[@name=\"ProPlus\"]/addresslist[@type=\"IPv4\"]/address/text()'"  <O365IPAddresses.xml >ProPlus_IPv4
+xmlsh -c "xpath '/products/product[@name=\"ProPlus\"]/addresslist[@type=\"URL\"]/address/text()'"   <O365IPAddresses.xml >ProPlus_URL
